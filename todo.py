9f841ee5c3de871a9ab8bfe08f6b26ecf0e27d34
kadelcode <kadelcode@gmail.com>
2025-02-15 13:05:12 +0100

    Create GUI Window

---
+++ todo.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# File to save tasks
+TASKS_FILE = "tasks.txt"
+
+# Create GUI Window
+root = tk.Tk()
+root.title("To-Do List App")
+root.geometry("400x500")
+
+# Run the Application
+root.mainloop()
